67e8561590b7831d96e2652248b181b1ac678c97
Shubham K. Sawant <shubhamksawant3@gmail.com>
2023-12-15 14:32:08 +0530

    Update check4.sh

--- check4.sh
+++ check4.sh
@@ -2,13 +2,14 @@
 
 #! bin/bash
 
-changed_files=$(git diff --name-only HEAD^ HEAD )
+changed_files=$(git diff --name-only)
 echo "$changed_files"
 # Check if there are any .xml files in the changed files
 if echo "$changed_files" | grep -qE '\.xml$'; then
     echo "Skipping the build because .xml files are present in the changes."
 else
     echo "Proceeding with the build."
+    exit 1
     # Add your build steps here
 fi
 
